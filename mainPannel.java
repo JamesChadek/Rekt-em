27e1117f7479dab057871db2f50d3e427dcb0a21
zomege <freneau.ellory@gmail.com>
2015-06-03 20:18:51 -0700

    GUI 2
    Window with right size. Title++. Organized and commented everything. GUI
    now compiles and runs in a minimal state

--- mainPannel.java
+++ mainPannel.java
@@ -3,18 +3,29 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class mainPannel extends JFrame implements ActionListener{
-	public static void main(String args[]) {
+	
+	String[] sites={"meme center","/b/","/g/","/pol/","string"};
+	JComboBox box=new JComboBox(sites);
+	JLabel dankResponse=new JLabel();
+	
+	public static void main(String args[]) {//pannel start
 		new mainPannel();
 	}
 	mainPannel() {
-		JLabel main = new JLabel("Hello World");
-		add(main);
+		//JLabel main = new JLabel("Hello World");
+		//add(main);
+		//Set pannel attributes
 		this.setSize(650,400);
-		String[] sites={"meme center","/b/","/g/","/pol/","string"};
+		this.setTitle("Internet danker than you can even imagine");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		JComboBox siteList = new JComboBox(sites);
 		siteList.setSelectedIndex(4);
 		siteList.addActionListener(this);
 		//swing stuff
+		this.setVisible(true);
+		
 	}
+	public void actionPerformed(ActionEvent e){}
 }
 
