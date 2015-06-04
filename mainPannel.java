86b21a7f5adc2c691b4465f48ee552b8d44e57e0
zomege <freneau.ellory@gmail.com>
2015-06-03 20:52:09 -0700

    GUI 3
    Created a functioning framework for adding images to the pannel (ie our
    mosaic). I put 2 place holder images in the meantime...
    
    For reference: the image read in is always "image.png" in the project
    directory.

--- mainPannel.java
+++ mainPannel.java
@@ -1,13 +1,26 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.*;
+import java.io.*;
 import javax.swing.*;
+import java.util.*;
+import javax.imageio.*;
 
 public class mainPannel extends JFrame implements ActionListener{
 	
 	String[] sites={"meme center","/b/","/g/","/pol/","string"};
 	JComboBox box=new JComboBox(sites);
 	JLabel dankResponse=new JLabel();
-	
+	public void paint(Graphics g){
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(new File("image.png"));
+		} catch (IOException e) {
+			System.out.println("fix it fix it fix it fix it fix it fix it fix it fix it fix it");
+		}
+		g.drawImage(img, 50, 50, this);
+		g.drawImage(img, 300, 50, this);
+	}
 	public static void main(String args[]) {//pannel start
 		new mainPannel();
 	}
@@ -18,10 +31,12 @@ public class mainPannel extends JFrame implements ActionListener{
 		this.setLayout(new FlowLayout());
 		this.setSize(650,400);
 		this.setTitle("Internet danker than you can even imagine");
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		//Combobox for site selection
 		box.setSelectedIndex(0);
 		box.addActionListener(this);
+		//Main image
+		
 		//swing stuff
 		add(box);
 		add(dankResponse);
