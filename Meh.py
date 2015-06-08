e73661abe7a94b469945b7c58a384667b8a81460
JamesChadek <unkillable9876@gmail.com>
2015-06-08 08:51:48 -0700

    Gave it the ability to work with Java

--- Meh.py
+++ Meh.py
@@ -2,9 +2,22 @@ import requests
 from lxml import html
 import sys
 import urlparse
+import os, shutil
 
-site = raw_input('Where should we scrape? > ')
-#str(Site)
+folder = 'Downloaded_Images'
+for the_file in os.listdir(folder):
+    file_path = os.path.join(folder, the_file)
+    try:
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+        #elif os.path.isdir(file_path): shutil.rmtree(file_path)
+    except Exception, e:
+        print e
+
+site = str(sys.argv[1]) 
+
+#site = raw_input('Where should we scrape? > ')
+#str(site)
 #'http://google.com'
 
 response = requests.get(site)
@@ -17,7 +30,10 @@ if not images:
 
 # Convert any relative urls to absolute urls
 images = [urlparse.urljoin(response.url, url) for url in images]
-print 'Found %s images' % len(images)
+print ('Found %s images' % len(images))
+
+#if not os.path.exists(directory):
+    #os.makedirs(directory)
 
 # Only download first 100
 for url in images[0:100]:
